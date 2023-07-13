737dc5b932dd9df8062dcec851a1f3581bb0ef6f
Aryan Malik <aryanhunny98@gmail.com>
2023-07-13 19:37:15 +0530

    Initial Commit

---
+++ contracts/EtherealToken.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EtherealToken is ERC20, Ownable{
+    // Setting token price - 1 ether
+    uint public tokenPrice;
+
+    // Setting the max supply - 1000 tokens
+    uint public maxSupply;
+
+    // Mapping checks whether the user already owns a token or not..
+    mapping(address => bool) public tokenRecord;
+
+    constructor() ERC20("EtherealToken", "MAG") {
+        tokenPrice = 0.01 ether;
+        maxSupply = 1000000000000000000000;
+    }
+
+    function mint() public payable{
+        require(totalSupply() + 1 <= maxSupply, "Sorry!! No more tokens");
+        require(tokenRecord[msg.sender] == false, "You already got a token!!");
+        require(msg.value == tokenPrice, "Your wallet doesn't consist of the valid amount for this token");
+        _mint(msg.sender, 1);
+        tokenRecord[msg.sender] = true;
+    }
+
+    function setMaxSupply(uint _maxSupply) public onlyOwner{
+        maxSupply = _maxSupply;
+    }
+
+    function setTokenPrice(uint _tokenPrice) public onlyOwner{
+        tokenPrice = _tokenPrice;
+    }
+
+    function withDraw() public onlyOwner{
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function returnState() public view returns(uint _maxSupply, uint _totalSupply, uint _tokenPrice){
+        return (maxSupply, totalSupply(), tokenPrice);
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+// We need to import these extensions in order for hardhat to function properly
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_RPC_URL,
+      accounts: [`0x${process.env.WALLET_PRIVATE_KEY}`],
+    },
+  },
+
+  // We don't need following ethescan block for deployment, but we need it for smart contract verification (upload).
+  etherscan: {
+    apiKey: process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY,
+  },
+};
