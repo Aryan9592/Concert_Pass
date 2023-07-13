ab82468a1175398cf281746fcc5a43038e974884
Aryan Malik <aryanhunny98@gmail.com>
2023-07-13 20:13:59 +0530

    trial

--- contracts/EtherealToken.sol
+++ contracts/EtherealToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EtherealToken is ERC20, Ownable{
+contract Ethereal is ERC20, Ownable{
     // Setting token price - 1 ether
     uint public tokenPrice;
 
