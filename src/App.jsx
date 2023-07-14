3743b4a97b7046f345a71e53c3651b17fb0a597b
Aryan Malik <aryanhunny98@gmail.com>
2023-07-15 00:13:48 +0530

    Some progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 import Header from './components/Header';
+import {useState, useEffect} from 'react'
+import { ethers } from 'ethers';
 
 function App() {
+  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [signer, setSigner] = useState(null)
+
+
+  const loadBlockchainData = async () => {
+    // This provider and signer be interacting with our blockchain
+    let _provider, _signer
+    try {
+      if(window.ethereum == null){
+        _provider = ethers.getDefaultProvider()
+      } else {
+        _provider = new ethers.BrowserProvider(window.ethereum)
+      }
+      _signer = await _provider.getSigner()
+      setProvider(_provider)
+      setSigner(_signer)
+    } catch (error) {
+      console.log("No wallet is Connected!!")
+    }
+
+    // affects when you change the account
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_accounts'})
+      if(accounts.length !== 0){
+        const account = await ethers.getAddress(accounts[0])
+        setAccount(account)
+      }  
+    })
+    
+    console.log(_signer)
+    // console.log(provider)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
     <div className="App">
-      <Header />      
+      <Header account={account} setAccount={setAccount}/>      
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Logo from "../images/logo.jpg"
+import {ethers} from 'ethers';
 
-export default function Header(){
+export default function Header({account, setAccount}){
+
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+        const account = ethers.getAddress(accounts[0])
+        setAccount(account)
+    }
+
     return(
         <header>
             <img src={Logo} alt="" width={40}/>
-            <button>Connect</button>
+            {account ? (
+                <button>
+                    {account.slice(0, 4) + '...' + account.slice(38, 42)}
+                </button>
+            ) : (
+                <button onClick={connectHandler}>
+                    Connect
+                </button>
+            )}
         </header>
     )
 }
