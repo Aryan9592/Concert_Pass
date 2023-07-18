fa60609441cba96774fa9a68e3142a5c9aaf7939
Aryan Malik <aryanhunny98@gmail.com>
2023-07-18 12:19:13 +0530

    Fixed Bugs - Connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import Header from './components/Header';
 import {useState, useEffect} from 'react'
 import { ethers } from 'ethers';
-import { message } from "antd"
+import { message } from "antd";
+import { formatBalance, formatChainAsNum } from './utils/info';
 import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
-  const [account, setAccount] = useState(null)
   const [provider, setProvider] = useState(null)
   const [signer, setSigner] = useState(null)
   const [messageApi, contextHolder] = message.useMessage()
 
   const [hasProvider, setHasProvider] = useState(null)
-  const initialState = { accounts: [] }
+  const initialState = { accounts: [], balance: "", chainId: "" }
   const [wallet, setWallet] = useState(initialState)
 
-  
+  const error_key = "error"
+
+  // This useEffect is for handling metamask accounts thing
   useEffect(() => {
     const refreshAccounts = (accounts) => {
       if (accounts.length > 0){
@@ -22,6 +24,10 @@ function App() {
       } else {
         setWallet(initialState)
       }
+    }
+
+    const refreshChain = (chainId) => {
+      setWallet((wallet) => ({...wallet, chainId}))
     }
 
     const getProvider = async () => {
@@ -33,6 +39,7 @@ function App() {
         })
         refreshAccounts(accounts)
         window.ethereum.on('accountsChanged', refreshAccounts)
+        window.ethereum.on('chainChanged', refreshChain)
       } else {
         messageApi.warning("Please Install Metamask!!")
       }
@@ -41,11 +48,35 @@ function App() {
     getProvider()
     return () => {
       window.ethereum?.removeListener('accountsChanged', refreshAccounts)
+      window.ethereum?.removeListener("chainChanged", refreshChain)
     }
   }, [])
 
   const updateWallet = async (accounts) => {
-    setWallet({accounts})
+    const balance = formatBalance(await window.ethereum.request({
+      method: "eth_getBalance",
+      params: [accounts[0], "latest"]
+    }))
+    const chainId = await window.ethereum.request({
+      method: "eth_chainId"
+    })
+    setWallet({accounts, balance, chainId})
+
+    // setting signers and providers
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const signer = await provider.getSigner()
+
+    setProvider(provider)
+    setSigner(signer)
+  }
+
+  const showError = (errorMsg) => {
+    messageApi.open({
+      error_key,
+      type: "error",
+      content: errorMsg,
+      duration: 2
+    })
   }
   
 
@@ -53,7 +84,7 @@ function App() {
     <>
       {contextHolder}
       <div className="App">
-        <Header wallet={wallet} updateWallet={updateWallet}/>     
+        <Header wallet={wallet} updateWallet={updateWallet} showError={showError}/>     
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+    import { message } from "antd";
 import Logo from "../images/logo.jpg"
 
-export default function Header({wallet, setWallet, updateWallet}){
+export default function Header({wallet, updateWallet, showError}){
+    const [messageApi, contextHolder] = message.useMessage()
+    const key = 'updatable'
+    const [isConnecting, setIsConnecting] = useState(null)
+    const [error, setError] = useState(false)
     
     const connectHandler = async () => {
-        let accounts = await window.ethereum.request({
+        setIsConnecting(true)
+        await window.ethereum.request({
             method: "eth_requestAccounts",
         })
-        updateWallet(accounts)
+        .then((accounts) => {
+            setError(false)
+            updateWallet(accounts)
+        })
+        .catch((error) => {
+            setError(true)
+            showError(error.message)
+        })
+        setIsConnecting(false)
     }
 
+    useEffect(() => {
+        const showMessage = () => {
+            if(isConnecting) {
+                messageApi.open({
+                    key,
+                    type: "loading",
+                    content: "Connecting..."
+                })
+            }
+            else if (isConnecting === false && !error){
+                messageApi.open({
+                    key,
+                    type: "success",
+                    content: "Connected!",
+                    duration: 2
+                })
+            }
+        }
+
+        showMessage()
+        return () => {
+            setIsConnecting(null)
+            setError(false)
+        }
+    }, [isConnecting])
+
     return(
-        <header>
-            <img src={Logo} alt="" width={40}/>
-            {wallet.accounts.length > 0 ? (
-                <button>
-                    {wallet.accounts[0].slice(0, 4) + '...' + wallet.accounts[0].slice(38, 42)}
-                </button>
-            ) : (
-                <button onClick={connectHandler}>
-                    Connect
-                </button>
-            )}
-        </header>
+        <>
+            {contextHolder}
+            <header>
+                <img src={Logo} alt="" width={40}/>
+                {wallet.accounts.length > 0 ? (
+                    <button>
+                        {wallet.accounts[0].slice(0, 4) + '...' + wallet.accounts[0].slice(38, 42)}
+                    </button>
+                ) : (
+                    <button onClick={connectHandler}>
+                        Connect
+                    </button>
+                )}
+            </header>
+        </>
     )
 }
